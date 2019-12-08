0279f99a3ec9910a873624010ff7b11da5315095
ekyo-pl <ekyo-pl@users.noreply.github.com>
2019-12-08 17:54:30 +0100

    Update en.php

--- lang/en.php
+++ lang/en.php
@@ -1,6 +1,7 @@
 <?php
 $lang['hitcounter']['name'] = 'Hit counter';
 $lang['hitcounter']['intro'] = 'A simple hit counter based on http://justintadlock.com/web-design/counter';
+$lang['hitcounter']['title'] = 'Hit counter admin page';
 $lang['hitcounter']['manage'] = 'Hit counter module management';
 $lang['hitcounter']['write_word'] = 'write a word';
 $lang['hitcounter']['write_word_info'] = 'enter the word you want to appear on the page - this field may be empty'; 
