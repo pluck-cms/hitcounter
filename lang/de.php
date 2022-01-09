87f53628e446f7721bebce2a58881f95ad96b64e
kalenderfamily <96819979+kalenderfamily@users.noreply.github.com>
2022-01-09 13:26:17 +0100

    Create de.php
    German translation based on en.php

---
+++ lang/de.php
@@ -0,0 +1,8 @@
+<?php
+$lang['hitcounter']['name'] = 'Besuchszaehler';
+$lang['hitcounter']['intro'] = 'Ein einfacher Besuchszaehler basierend auf http://justintadlock.com/web-design/counter';
+$lang['hitcounter']['title'] = 'BesuchszaehlerAdminbereich';
+$lang['hitcounter']['manage'] = 'Besuchszaehler Modulverwaltung';
+$lang['hitcounter']['write_word'] = 'Schreibe ein Wort';
+$lang['hitcounter']['write_word_info'] = 'Lege die Bezeichnung fest, welche auf der Webseite erscheinen soll - dieses Feld kann auch leer bleiben'; 
+?>
